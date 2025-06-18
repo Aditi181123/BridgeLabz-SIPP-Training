e343f865ee4bb4c3e3ca8ee8b9d054039317ec9f
Aditi181123 <tanuaditi04@gmail.com>
2025-06-18 11:22:50 +0530

    Aditi/ add new files in method

---
+++ Method/Calculate_windtemp.java
@@ -0,0 +1,25 @@
+package Method;
+import java.util.*;
+public class Calculate_windtemp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter temperature and wind speed :");
+		
+		double temp = sc.nextDouble();
+		double windspeed = sc.nextDouble();
+		
+		double windchill = calculateWindChill(temp, windspeed);
+		System.out.println(windchill);
+		
+	}
+	public static double calculateWindChill(double temp, double windspeed) {
+		double a = Math.pow(windspeed, 0.16);
+		double c = 0.4275 * temp - 35.75;
+		double b = 35.74 + 0.6215 * temp + c * a;
+		return b;
+	}
+
+}
---
+++ Method/check_num.java
@@ -0,0 +1,29 @@
+package Method;
+import java.util.*;
+public class check_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter a number:");
+		int  n = sc.nextInt();
+		
+		int check = checknum(n);
+		System.out.println(check);
+	}
+
+	private static int checknum(int n) {
+		// TODO Auto-generated method stub
+		if(n < 0) {
+			return -1;
+		}
+		else if(n == 0) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	
+
+}
