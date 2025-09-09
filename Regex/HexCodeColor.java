d50ccd15cb2b90d96657c680b90685b62c3324d6
Aditi181123 <tanuaditi04@gmail.com>
2025-09-09 21:46:08 +0530

    [Aditi Mittal] Regex Expressions

---
+++ Regex/HexCodeColor.java
@@ -0,0 +1,22 @@
+package Regex;
+
+import java.util.Scanner;
+
+public class HexCodeColor {
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter hex color code : ");
+        String colorCode = sc.nextLine();
+        
+        String regex = "^#[A-Fa-f0-9]{6}$";
+        sc.close();
+        
+        if (colorCode.matches(regex)) {
+            System.out.println("\"" + colorCode + "\" → Valid");
+        } else {
+            System.out.println("\"" + colorCode + "\" → Invalid");
+        }
+    }
+}
