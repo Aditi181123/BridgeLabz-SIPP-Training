2e1c624baeb696cdc3f06564a7aa1dd36eceff20
Aditi181123 <tanuaditi04@gmail.com>
2025-09-09 21:33:01 +0530

    Added to Lambda Expressions

---
+++ Lambda/Invoice.java
@@ -0,0 +1,13 @@
+package Lambda;
+public class Invoice {
+    String transactionId;
+
+    Invoice(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice generated for Transaction ID : " + transactionId;
+    }
+}
